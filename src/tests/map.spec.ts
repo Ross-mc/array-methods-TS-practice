6a6ce48fd39bce1e26b45aa54a055aa7c21a040d
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-08-23 16:50:58 +0100

    finished tests for customMap

--- src/tests/map.spec.ts
+++ src/tests/map.spec.ts
@@ -1,9 +1,9 @@
 import {customMap} from "../map";
-import { strArr, numberArr } from "../helpers";
-import {mapCallback} from "../types"
+import { strArr, numberArr, multiplyByFive } from "../helpers";
+import {mapCallback} from "../types";
 
 describe('customMap.ts', () => {
-  it('It should call the callback function for each element in the array', () => {
+  it('should call the callback function for each element in the array', () => {
     //Given
     const cb = jest.fn();
 
@@ -12,5 +12,40 @@ describe('customMap.ts', () => {
 
     //Then
     expect(cb).toBeCalledTimes(numberArr.length)
-  })
+  });
+
+  it('should call the callback function with the current value, the index and a reference to the original array', () => {
+    //Given
+    const cb = jest.fn();
+    const arr = [1,2,3];
+
+    //When
+    customMap(arr, cb);
+
+    //Then
+    expect(cb).toBeCalledWith(1, 0, arr);
+  });
+
+  it('should return an array of equal length to the original array', () => {
+    //Given
+    const cb = jest.fn().mockReturnValue(42);
+
+    //When
+    const result = customMap(numberArr, cb);
+
+    //Then
+    expect(Array.isArray(result)).toBeTruthy();
+    expect(result).toHaveLength(numberArr.length);
+  });
+
+  it('should place the correct value for the first element of the original array into the first element of new array', () => {
+    //Given
+    const arr = [1,2,3]
+    
+    //When
+    const result = customMap(arr, multiplyByFive);
+
+    //Then
+    expect(result[0]).toBe(5);
+  });
 })
