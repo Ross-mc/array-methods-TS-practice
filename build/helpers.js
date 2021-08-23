40e4710dae02d9259e696fd99b3abe8c161cfb39
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-08-23 11:20:32 +0100

    basic folder set up, created type safe custom map function

---
+++ build/helpers.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.strArr = exports.numberArr = void 0;
+exports.numberArr = [1, 2, 3, 4, 5, 10, 20, 50, 100];
+exports.strArr = ['Hello', 'this', 'is', 'a', 'string', 'array'];
---
+++ src/helpers.ts
@@ -0,0 +1,3 @@
+export const numberArr: number[] = [1,2,3,4,5,10,20,50,100];
+
+export const strArr: string[] = ['Hello', 'this', 'is', 'a', 'string', 'array'];
